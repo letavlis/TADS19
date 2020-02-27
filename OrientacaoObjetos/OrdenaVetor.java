57ab3adf5d74bc7e4e3c24da8d3537ea556591eb
letavlis <bruna.letavlis@outlook.com>
2020-02-27 14:25:07 -0300

    Ordenei um vetor em java

--- OrientacaoObjetos/OrdenaVetor.java
+++ OrientacaoObjetos/OrdenaVetor.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class OrdenaVetor{
     public static void main(String[] args) {
         
-        int i, tam, j, flag=0;
+        int i, tam, j, flag=0, aux;
         Scanner scan = new Scanner(System.in);
     
         System.out.printf("Informe a quantidade de numeros quer digitar: ");
@@ -11,24 +11,43 @@ public class OrdenaVetor{
 
         int[] arr;
         arr = new int[tam];
-        System.out.println("\nDigite %d numeros", tam)
+        System.out.printf("\nDigite %d numeros\n", tam);
         for(i = 0; i < tam; i++){
-            System.ou.println(""+d+"° numero: "+(i+1));
+            System.out.printf("%d° numero: ", i+1);
             arr[i] =  scan.nextInt();
+            System.out.println();
         }
         
+        System.out.print("\nO vetor digitado: ");
+
         for(i = 0; i < tam; i++){
-            for(j = i+1; j < tam;){
+                System.out.printf("%d ", arr[i]);
+        }
+
+        for(i = 0; i < tam; i++){
+           j = i+1;
+           while(j < tam-1){
                 if(arr[i]>arr[j]){
                     flag++;
                 }
+                j++;
             }
         }
         if (flag != 0){
-            //Ordene o vetor
-        }
-        else{
-            //Printe mensagem de vetor ordenado
-        }
+            for (i = 1; i < tam; i++){
+                    aux = arr[i];
+                    j=i-1;
+                    while(j >= 0 && aux < arr[j]){
+                            arr[j+1] = arr[j];
+                            j--;
+                    }
+                    arr[j+1] = aux;
+            }
+            System.out.print("\n\nO vetor ordenado: ");
+            for(i=0; i < tam; i++){
+                System.out.printf("%d ", arr[i]);
+            }
+       }
+       System.out.println();
     }
 }

8d8cc0ae4a063a4322d182952a57e7a35f8bfb9e
letavlis <bruna.letavlis@outlook.com>
2020-03-11 00:37:55 -0300

    Quase terminando

--- OrientacaoObjetos/ExCarro/Main.java
+++ OrientacaoObjetos/ExCarro/Main.java
@@ -5,7 +5,7 @@ public class Main{
 		
 		Carro carro1, carro2, carro3;
 		Scanner scan = new Scanner(System.in);
-		int escolha;
+		int escolha, i, x;
 
 		carro1 = new Carro();
 		carro1.marca = "Dodge";
@@ -40,7 +40,31 @@ public class Main{
 		System.out.print("\n\nEscolha um carro: ");
 		escolha = scan.nextInt():
 
-		if (ecolha == 1){}
+		if (ecolha == 1){
+			do{
+				escolha = 0;
+				System.out.println("-Menu-");
+				System.out.println("1.Acelerar");
+				System.out.println("2.Abastecer");
+				System.out.println("3.Estado atual")
+				System.out.println("4.Sair");
+				System.out.println();
+				System.out.print("-Escolha uma opção: ");
+				escolha = scan.nextInt();
+				if (escolha == 1) {
+					carro1.acelerar();
+				}
+				else if (escolha == 2){
+					System.out.println("Quantos litros deseja abastecer? ")
+					x = scan.nextInt();
+					carro1.abastecer(x);
+				}
+				else if (escolha == 3){
+					System.out.println("Velociadade atual: "+i+""+carro1.velociadade);
+					System.out.println("Quantidade de combustivel no tanque: "+i+""+carro1.combustivel);
+				}
+			}while(i == 1)
+		}
 		else if (ecolha == 1){}
 		else if (ecolha == 1){}
 

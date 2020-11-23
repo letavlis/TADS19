224108651c18358e45dab83131ccbaeeccecaa99
letavlis <avlis265153>
2020-11-23 10:31:21 -0300

    rde2 terminei

---
+++ OrientacaoObjetos/rde2/Zoo/src/pacote1/terrestre/Terrestre.java
@@ -0,0 +1,10 @@
+package pacote1.terrestre;
+
+import pacote1.Animal;
+
+public abstract class Terrestre extends Animal {
+
+    public Terrestre(int codigo, double peso, String nome) {
+        super(codigo, peso, nome);
+    }
+}
---
+++ OrientacaoObjetos/rde2/Zoo/src/pacote1/terrestre/semPelos/SemPelos.java
@@ -0,0 +1,9 @@
+package pacote1.terrestre.semPelos;
+
+import pacote1.terrestre.Terrestre;
+
+public abstract class SemPelos extends Terrestre {
+    public SemPelos(int codigo, double peso, String nome) {
+        super(codigo, peso, nome);
+    }
+}

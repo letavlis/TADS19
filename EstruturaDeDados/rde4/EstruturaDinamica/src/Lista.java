fcd55f87e28f41a7cb86cd50a84409c4beed4c73
Bruna Leticia <bruna.letavlis@outlook.com>
2020-11-30 20:39:06 -0300

    inserindo print reverso

--- EstruturaDeDados/rde4/EstruturaDinamica/src/Lista.java
+++ EstruturaDeDados/rde4/EstruturaDinamica/src/Lista.java
@@ -1,18 +1,21 @@
 public class Lista{
-	private Nodo prim;
+		private Nodo prim;
 
-	public void criaLista(){
-		prim = null;
-	}
-	public void addInicio(int i){
-		Nodo novo = new Nodo();//Alocar memoria para nó
-		novo.setInfo(i);//Define novo valor
-		novo.setProx(prim);//Apont para o que ja existe
-		prim = novo;//define o novo como primeiro
-	}
-	public void print(){
-		for(Nodo n = prim; n != null; n = n.getProx()){
-			System.out.println(n.getInfo());
+		public void criaLista(){
+			prim = null;
 		}
-	}
+		public void addInicio(int i){
+			Nodo novo = new Nodo();//Alocar memoria para nó
+			novo.setInfo(i);//Define novo valor
+			novo.setProx(prim);//Apont para o que ja existe
+			prim = novo;//define o novo como primeiro
+		}
+		public void print(){
+			for(Nodo n = prim; n != null; n = n.getProx()){
+				System.out.println(n.getInfo());
+			}
+		}
+		public void printReverso(){
+			printAux(prim);
+		}
 }

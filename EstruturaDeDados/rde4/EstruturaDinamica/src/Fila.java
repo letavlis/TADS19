2abd04c5b884ed9e052cc2b828c1f07b1411b80a
letavlis <avlis265153>
2020-12-05 10:56:50 -0300

    criei classe fila 05122020

---
+++ EstruturaDeDados/rde4/EstruturaDinamica/src/Fila.java
@@ -0,0 +1,36 @@
+public class Fila {
+    private Nodo head;
+    private Nodo tail;
+
+    public void Fila(){//construtor deixa a fila zerada
+        tail = null;
+        head = null;
+    }
+
+    public Nodo getTail() {//retorna a cauda
+        return tail;
+    }
+
+    public Nodo getHead() {//retorna a cabeça
+        return head;
+    }
+
+    public void enqueue(int i){
+        Nodo novo = new Nodo();// inicia um novo nó
+        novo.setInfo(i);// recebe o valor da info
+        if(tail == null){//verifica se a fila está vazia
+            head = novo;//define como sendo o "novo"
+            tail = novo;// define como sendo o "novo"
+        }
+        else{//se não estiver vazia
+            tail.setProx(novo);// aloca no final da fila
+            tail = novo;//atualiza o nó cauda
+        }
+    }
+    public void dequeue(){
+        if(head == null){//testa se a cabeça da fila está vazia
+            System.out.println("Fila vazia");
+        }
+        else head=head.getProx();//senão atualiza a fila para começar no proximo elemento
+    }
+}

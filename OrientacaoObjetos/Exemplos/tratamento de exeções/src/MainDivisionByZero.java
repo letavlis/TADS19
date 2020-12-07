fb83c4cea7081a16ed8f677e22712c92d4248091
letavlis <avlis265153>
2020-12-07 16:43:56 -0300

    exemplo exceção Divisão por zero

--- OrientacaoObjetos/Exemplos/tratamento de exeções/src/MainDivisionByZero.java
+++ OrientacaoObjetos/Exemplos/tratamento de exeções/src/MainDivisionByZero.java
@@ -4,8 +4,12 @@ public class MainDivisionByZero {
         int valor = 10;
         int divisor = 0;
 
-        int resultado = valor/divisor;
-        System.out.println("O resultado é: "+resultado);
+        try {
+            int resultado = valor/divisor;
+            System.out.println("O resultado é: "+resultado);
+        }catch(ArithmeticException e){
+            System.out.println("Ocorreu uma exceção:/ by zero");
+        }
         System.out.println("Fim...");
     }
 }

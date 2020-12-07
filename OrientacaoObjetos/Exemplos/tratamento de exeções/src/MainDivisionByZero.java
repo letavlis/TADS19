4c74ab9f9224bc874e989509709f07bdfa6ee21c
letavlis <avlis265153>
2020-12-07 16:24:19 -0300

    exemplo Divisão por zero

---
+++ OrientacaoObjetos/Exemplos/tratamento de exeções/src/MainDivisionByZero.java
@@ -0,0 +1,11 @@
+public class MainDivisionByZero {
+
+    public static void main(String[] args) {
+        int valor = 10;
+        int divisor = 0;
+
+        int resultado = valor/divisor;
+        System.out.println("O resultado é: "+resultado);
+        System.out.println("Fim...");
+    }
+}

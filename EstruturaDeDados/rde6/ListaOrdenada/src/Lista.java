3d480acfaa07cf907bfd49f6371adff0bba086f0
letavlis <avlis265153>
2020-12-14 19:11:27 -0300

    rde4

---
+++ EstruturaDeDados/rde6/ListaOrdenada/src/Lista.java
@@ -0,0 +1,60 @@
+public class Lista {
+    private Nodo head;
+    private Nodo tail;
+
+    public Lista(){
+        head = null;
+        tail = null;
+    }
+    public void print(Nodo n){
+        if (n!=null){
+            System.out.println(n.getInfo());
+            print(n.getProx());
+        }
+    }
+    public Nodo getRaiz(){
+        return head;
+    }
+    public void addInicio(int i){
+        Nodo novo = new Nodo();
+        novo.setInfo(i);
+        novo.setProx(head);
+        head = novo;
+    };
+    public void addFinal(int i){
+        Nodo novo = new Nodo();
+        novo.setInfo(i);
+        novo.setProx(tail);
+        tail = novo;
+    };
+    public void addInOrder(int i){
+        if (head == null){
+            addInicio(i);
+            tail = head;
+        }
+        else if (head.getInfo() > i) addInicio(i);
+        else if(tail.getInfo() < i) addFinal(i);
+        else{
+            Nodo novo = new Nodo();
+            Nodo x;
+            novo.setInfo(i);
+            x = head;
+            while(x.getProx().getInfo() < novo.getInfo()){
+                x = x.getProx();
+                if(x.getProx() == null)break;
+            }
+            novo.setProx(x.getProx());
+            x.setProx(novo);
+        }
+    }
+    public void removeInicio(){
+        head = head.getProx();
+    }
+    public void printReverso(Nodo n){
+        if (n!=null){
+            printReverso(n.getProx());
+            System.out.println(n.getInfo());
+        }
+    }
+
+}
--- EstruturaDeDados/rde6/ListaOrdenada/src/Main.java
+++ EstruturaDeDados/rde6/ListaOrdenada/src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
 
     public static void main(String[] args) {
+        Lista lista;
+        System.out.println("Lista Ordenada");
+        lista = new Lista();
+        lista.addInOrder(33);
+        lista.addInOrder(22);
+        lista.addInOrder(45);
+        lista.addInOrder(70);
+        lista.addInOrder(30);
+        lista.addInOrder(20);
+        System.out.println("Imprimindo valores:\n");
+        lista.print(lista.getRaiz());
+        System.out.println("\nImprimindo valores inversamente:\n");
+        lista.printReverso(lista.getRaiz());
     }
 }

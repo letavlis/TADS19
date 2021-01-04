debb795475919f2976eba6f0bce473b235b5539e
letavlis <avlis265153>
2021-01-04 20:51:33 -0300

    criei classe veiculo

---
+++ OrientacaoObjetos/rde-trabalhosemestral/src/modelos/Veiculo.java
@@ -0,0 +1,58 @@
+package modelos;
+
+public class Veiculo {
+
+    private int codigo;
+    private String placa;
+    private int ano;
+    private String modelo;
+    private String cliente;
+
+    public Veiculo(int codigo, String placa, int ano, String modelo, String cliente) {
+        this.codigo = codigo;
+        this.placa = placa;
+        this.ano = ano;
+        this.modelo = modelo;
+        this.cliente = cliente;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(String cliente) {
+        this.cliente = cliente;
+    }
+}

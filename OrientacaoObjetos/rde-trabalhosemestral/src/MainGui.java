fcca2f8eeaa4a90d98251f48bd1960169e55fa34
letavlis <avlis265153>
2021-01-15 02:16:09 -0300

    interface

--- OrientacaoObjetos/rde-trabalhosemestral/src/MainGui.java
+++ OrientacaoObjetos/rde-trabalhosemestral/src/MainGui.java
@@ -1,6 +1,7 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 public class MainGui extends Application{
     public static void main(String[] args){
@@ -8,10 +9,19 @@ public class MainGui extends Application{
     }
     @Override
     public void start(Stage stage) throws Exception{
-        VBox root = new VBox();
-        root.setStyle("-fx-background-color: gray");
-        Scene scene = new Scene(root, 1000,500);
+
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("fxml/JanelaCadVeiculo.fxml"));
+
+        Parent root = loader.load();
+        //VBox root = new VBox();
+        //root.setStyle("-fx-background-color: gray");
+
+        Scene scene = new Scene(root,400,600);
+
         stage.setScene(scene);
+        stage.setTitle("Gerenciador de Mecanica");
+
         stage.show();
     }
 }
--- OrientacaoObjetos/rde-trabalhosemestral/src/janelas/JanelaCadVeiculo.java
+++ OrientacaoObjetos/rde-trabalhosemestral/src/janelas/JanelaCadVeiculo.java
@@ -1,4 +1,38 @@
 package janelas;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
+
 public class JanelaCadVeiculo {
+    @FXML
+    private TextField tfPlaca;
+    @FXML
+    private TextField tfModelo;
+    @FXML
+    private TextField tfCliente;
+
+    private void cadastrar(){
+        String placa = tfPlaca.getText();
+        String modelo = tfModelo.getText();
+        String cliente = tfCliente.getText();
+
+        if (placa.isBlank() || placa.isEmpty()){
+            mensagem("Placa invalida!!");
+            return;
+        }
+        if (modelo.isBlank() || modelo.isEmpty()){
+            mensagem("Modelo invalido!!");
+            return;
+        }
+        if (cliente.isBlank() || cliente.isEmpty()){
+            mensagem("cliente invalida!!");
+            return;
+        }
+    }
+    private void mensagem(String msg){
+        Alert alert = new Alert(Alert.AlertType.ERROR,msg);
+        alert.showAndWait();
+    }
+
 }

6865800a169064db124ad4043fcb581f361cb41b
letavlis <avlis265153>
2021-01-15 01:04:22 -0300

    fazendo interface

--- OrientacaoObjetos/rde-trabalhosemestral/src/MainGui.java
+++ OrientacaoObjetos/rde-trabalhosemestral/src/MainGui.java
@@ -2,7 +2,7 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-public class Principal extends Application{
+public class MainGui extends Application{
     public static void main(String[] args){
         Application.launch(args);
     }

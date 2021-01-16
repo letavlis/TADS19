5726db3064eb4edfaa00d136c3fa0ac269f8984e
Bruna Leticia <bruna.letavlis@outlook.com>
2021-01-15 21:11:18 -0300

    cadastro de pintura

---
+++ OrientacaoObjetos/rde-trabalhosemestral/src/Mecanica/janelas/JanelaCadPintura.java
@@ -0,0 +1,18 @@
+package Mecanica.janelas;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class JanelaCadPintura {
+    @FXML
+    private TextField tfDescricao;
+    @FXML
+    private  TextField tfCor;
+    @FXML
+    private TextField tfVMaoDeObra;
+    @FXML
+    private  TextField tfCera;
+    @FXML
+    private  TextField tfTinta;
+
+}

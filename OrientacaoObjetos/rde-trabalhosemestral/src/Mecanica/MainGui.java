0df2dabff10232586e21502711d74b99df7fcffb
letavlis <avlis265153>
2021-01-15 23:03:58 -0300

    Cadastra pintura interface

--- OrientacaoObjetos/rde-trabalhosemestral/src/Mecanica/MainGui.java
+++ OrientacaoObjetos/rde-trabalhosemestral/src/Mecanica/MainGui.java
@@ -1,7 +1,6 @@
 package Mecanica;
 
 import Mecanica.controles.Mecanica;
-import Mecanica.janelas.JanelaCadVeiculo;
 import Mecanica.janelas.Principal;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -17,6 +16,7 @@ public class MainGui extends Application{
 
     public static final String PRINCIPAL = "/fxml/Principal.fxml";
     public static final String CADVEICULO = "/fxml/JanelaCadVeiculo.fxml";
+    public static final String CADPINTURA = "/fxml/JanelaCadPintura.fxml";
 
     private Mecanica mecanica;
 
--- OrientacaoObjetos/rde-trabalhosemestral/src/Mecanica/janelas/JanelaCadPintura.java
+++ OrientacaoObjetos/rde-trabalhosemestral/src/Mecanica/janelas/JanelaCadPintura.java
@@ -1,6 +1,8 @@
 package Mecanica.janelas;
 
+import Mecanica.controles.Mecanica;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 
 public class JanelaCadPintura {
@@ -14,5 +16,47 @@ public class JanelaCadPintura {
     private  TextField tfCera;
     @FXML
     private  TextField tfTinta;
+
+    Mecanica mecanica;
+
+    public JanelaCadPintura(Mecanica mecanica){
+        this.mecanica = mecanica;
+    }
+
+    @FXML
+    private void cadastrar(){
+        String descricao = tfDescricao.getText();
+        String cor = tfCor.getText();
+
+
+        if (descricao.isBlank() || descricao.isEmpty()){
+            //TODO melhorar a validação
+            mensagem(Alert.AlertType.ERROR,"Descricao invalida!!");
+            return;
+        }
+        if (cor.isBlank() || cor.isEmpty()){
+            //TODO melhorar a validação
+            mensagem(Alert.AlertType.ERROR,"Cor invalida!!");
+            return;
+        }
+
+        //cadastrar veiculo
+       /* if(!mecanica.cadastraVeiculo(placa, ano, modelo, cliente)){
+            mensagem(Alert.AlertType.ERROR,"Veiculo não cadastrado");
+        }
+        else {
+            mensagem(Alert.AlertType.INFORMATION, "Veiculo cadastrado");
+            MainGui.mudaCena(MainGui.PRINCIPAL, (aClass) -> new Principal(mecanica));
+        }*/
+
+    }
+    private void mensagem(Alert.AlertType type ,String msg){
+        Alert alert = new Alert(type,msg);
+        alert.showAndWait();
+    }
+    public void limpar(){
+        tfDescricao.setText("");
+        tfCor.setText("");
+    }
 
 }

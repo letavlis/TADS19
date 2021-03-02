7f65e75a10eccd5fd058175eec7fb7a35e39cff9
letavlis <avlis265153>
2021-03-02 20:59:38 -0300

    02/03

---
+++ OrientacaoObjetos/rde-trabalhosemestral/src/Mecanica/modelos/ExecutaServico.java
@@ -0,0 +1,31 @@
+import java.io.Serializable;
+
+public class ExecutaServico implements Serializable{
+	private int codigo;
+	private String data;
+	private Veiculo veiculo;
+	private Servico servico;
+	private double total;
+
+	public ExecutaServico(int codigo, Veiculo veiculo, Servico servico){
+		this.codigo = codigo;
+		this.veiculo = veiculo;
+		this.servico = servico;
+	}
+
+	public int getCodigo(){
+		return this.codigo;
+	}
+
+	public Veiculo getVeiculo(){
+		return this.veiculo;
+	}
+
+	public Servico getServico(){
+		return this.servico;
+	}
+
+	public double getTotal(){
+		return this.total;
+	}
+}
